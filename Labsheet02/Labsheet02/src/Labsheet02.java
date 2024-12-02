cfb0ccd197df40e308636e01662efaebe77aea02
Kamolthrt Praditphruek <kongkooks@gmail.com>
2024-12-02 13:24:22 +0700

    Labsheet02 Example

---
+++ Labsheet02/Labsheet02/src/Labsheet02.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+
+import java.text.*;
+import java.text.DecimalFormat;
+public class Labsheet02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int BUFFET = 299;
+		double discount;
+		int isMember;
+		DecimalFormat frm = new DecimalFormat("#,###.00");
+		int numberofCustomer = Integer.parseInt(
+				JOptionPane.showInputDialog("How many customer per bill?"));
+		double totalPrice = numberofCustomer * BUFFET;
+		do {
+		        isMember = JOptionPane.showConfirmDialog(null,
+				"Total Price is " + totalPrice+ "baht."+
+		        "\nDo you have a member card ");
+		}while(isMember ==2);
+		if (isMember==0) {
+			discount = totalPrice*0.90;
+			JOptionPane.showConfirmDialog(null,"Amount to pe paid is " + frm.format(discount) + "Baht");
+		}
+		else if (isMember == 1)
+		{
+			JOptionPane.showConfirmDialog(null,"Amount to pe paid is " + frm.format(totalPrice)+"Baht");
+		}
+			
+			
+		
+		
+		
+
+	}
+
+}
---
+++ Labsheet02/Labsheet02/src/salesperson.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class salesperson {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Declare constants
+		 final double COMMISSION_RATE = 0.15;
+		 final int SENTINEL = -1; // Terminating value for input
+		 final double BASE_SALARY = 10000.0;
+		 // Declare variables
+		 int sales; // Input gross sales
+		 double salary; // Salary to be computed
+		 Scanner input = new Scanner(System.in);
+		 
+		 while(true) {
+				 System.out.print("Enter gross sales for the salesperson(or -1 to end): ");
+				 sales = input.nextInt();
+				 if(sales==SENTINEL) {
+					 break;
+					 
+				 }
+				 salary = BASE_SALARY + (sales *  COMMISSION_RATE);
+				 
+				 System.out.printf("The salesperson's salary is $%,.2f",salary);
+				 System.out.println();
+	         }
+		         System.out.print("bye");
+		 System.out.printf("All salesperaon's salary have been computed.");
+	}
+	
+
+}
