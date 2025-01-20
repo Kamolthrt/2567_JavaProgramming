3bb571f70c785c4c4df7d235b5ba3d7921613f24
Kamolthrt Praditphruek <kongkooks@gmail.com>
2025-01-20 12:13:33 +0700

    Lab 07

---
+++ Lab 07/LabSheet07/src/TestRectangle.java
@@ -0,0 +1,29 @@
+
+public class TestRectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Rectangle r1 = new Rectangle();
+//System.out.println(r1.toString());
+System.out.println(r1);
+	
+//call 
+Rectangle r2 = new Rectangle(1.2f,3.4f);
+System.out.println(r1);
+System.out.println();
+
+//test
+r1.setLength(5.6f);
+r1.setWidth(7.8f);
+System.out.println(r1);
+System.out.println("Length is"+ r1.getLength());
+System.out.println("Width is"+ r1.getWidth());
+System.out.println();
+
+//test get area
+
+System.out.printf("area is %.2f%n", r1.getArea());
+System.out.printf("perimeter is: %.2f", r1.getPerimeter());
+	}
+
+}
