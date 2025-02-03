f6343d44c3eadfd398ebc830ed23e4fdb3f41d23
Kamolthrt Praditphruek <kongkooks@gmail.com>
2025-02-03 14:27:30 +0700

    Lab08

--- Lab 08/Labsheet8/src/StockProduct.java
+++ Lab 08/Labsheet8/src/StockProduct.java
@@ -6,24 +6,25 @@ public class StockProduct {
 	       product[] productList = new product[4];
 	       for (int i = 0; i < productList.length; i++) {
 	           productList[i] = new product();
-	           System.out.print("Input product Id   : ");
+	           System.out.print("Input product Id    : ");
 	           String id = scan.next();
 	           productList[i].setId(id);
-	           System.out.print("Input product Unit : ");
+	           System.out.print("Input product Unit  : ");
 	           int unit = scan.nextInt();
 	           productList[i].setUnit(unit);
-	           System.out.print("Input product Price: ");
+	           System.out.print("Input product Price : ");
 	           double price = scan.nextDouble();
 	           productList[i].setPrice(price);
 	           System.out.println();
 	       }
 	       double totalPrice = 0;
-	       System.out.println("\n--------------------------------------");
+	       System.out.println("----------------------------------------");
 	       for (product product : productList) {
 	           double productTotal = product.calculate();
 	           totalPrice += productTotal;
 	           System.out.println("Product ID: " + product.getId() + ", Total price = " + productTotal + " baht.");
 	       }
+	       System.out.println("----------------------------------------");
 	       System.out.println("Total price of all products is " + totalPrice + " baht.");
 	     
 	       
